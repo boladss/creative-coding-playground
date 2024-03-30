b1cf38d8d9cf548d43d1f705aba04af116fea769
boladss <anton.boladss@gmail.com>
2024-03-30 17:02:40 +0800

    style: improve css, allow for wrapping

--- src/script.ts
+++ src/script.ts
@@ -8,7 +8,6 @@ function init() {
   let context1 = canvas1.getContext('2d') as CanvasRenderingContext2D;
   const width1 = canvas1.width;
   const height1 = canvas1.height;
-  console.log("test");
   initArcs(context1, width1, height1);
 }
 
